457cd209d673611c9895f9765abb24c515e3fac0
Turiano Romero <romero.guzman18@gmail.com>
2021-01-10 17:31:51 -0500

    probando ejemplo de expo Admob

--- App.js
+++ App.js
@@ -2,11 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  PublisherBanner,
+  AdMobRewarded,
+  setTestDeviceIDAsync,
+} from "expo-ads-admob";
+
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Turiano el capo</Text>
       <StatusBar style="auto" />
+
+      {/* <AdMobBanner
+        bannerSize="fullBanner"
+        adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+        servePersonalizedAds // true or false
+        // onDidFailToReceiveAdWithError={this.bannerError}
+      /> */}
+
+      <PublisherBanner
+        bannerSize="fullBanner"
+        adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+        // onDidFailToReceiveAdWithError={this.bannerError}
+        // onAdMobDispatchAppEvent={this.adMobEvent}
+      />
     </View>
   );
 }
